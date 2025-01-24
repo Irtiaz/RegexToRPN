d11688aaaf4d3a6cf85bb9117bfb590b3fe4714a
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-01-24 14:57:30 +0600

    added pop function in stack interface

--- main.c
+++ main.c
@@ -6,13 +6,9 @@
 
 #include "stack.h"
 
-
 #define MAX_TOKEN_LENGTH 5
 
-typedef enum  {
-  LEFT,
-  RIGHT
-} Associativity;
+typedef enum { LEFT, RIGHT } Associativity;
 
 typedef struct {
   char symbol;
@@ -20,12 +16,13 @@ typedef struct {
 } Operator;
 
 Operator operators[] = {
-  {'*', LEFT},
-  {'+', LEFT},
-  {'-', LEFT},
+    {'*', LEFT},
+    {'+', LEFT},
+    {'-', LEFT},
 };
 
-void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t number_of_tokens);
+void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH],
+                                size_t number_of_tokens);
 int token_to_operator_index(const char *token);
 size_t get_precedence(Operator operator);
 
@@ -37,8 +34,8 @@ int main(void) {
   return 0;
 }
 
-
-void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t number_of_tokens) {
+void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH],
+                                size_t number_of_tokens) {
   size_t i;
 
   Operator *operator_stack = NULL;
@@ -54,7 +51,10 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
       size_t precedence_of_x = get_precedence(x);
 
       /*
-       While there is an operator (y) at the top of the operators stack and either (x) is left-associative and its precedence is less or equal to that of (y), or (x) is right-associative and its precedence is less than (y)
+       While there is an operator (y) at the top of the operators stack and
+       either (x) is left-associative and its precedence is less or equal to
+       that of (y), or (x) is right-associative and its precedence is less than
+       (y)
       */
 
       while (!stack_is_empty(operator_stack)) {
@@ -64,15 +64,19 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
         int x_is_left_associative = x.associativity == LEFT;
         int x_is_right_associative = x.associativity == RIGHT;
         int x_precedence_is_less_than_y = precedence_of_x < precedence_of_y;
-        int x_precedence_is_less_or_equal_to_y = precedence_of_x <= precedence_of_y;
+        int x_precedence_is_less_or_equal_to_y =
+            precedence_of_x <= precedence_of_y;
 
-        if ((x_is_left_associative && x_precedence_is_less_or_equal_to_y) || (x_is_right_associative && x_precedence_is_less_than_y)) {
+        if ((x_is_left_associative && x_precedence_is_less_or_equal_to_y) ||
+            (x_is_right_associative && x_precedence_is_less_than_y)) {
           /* pop y from the stack and add it to the output buffer */
-          stack_delete_top(operator_stack);
+          (void)stack_pop(operator_stack);
           printf("%c\n", y.symbol);
         }
 
-        else break;
+        else {
+          break;
+        }
       }
 
       /* Push x on the operator stack */
@@ -85,34 +89,32 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
       if (parenthesis_token == '(') {
         /* Open parenthesis encountered, push its position */
         int current_index_of_operator_stack = stack_length(operator_stack);
-        stack_push(open_parenthesis_indices_in_operator_stack, current_index_of_operator_stack);
-      }
-      else {
+        stack_push(open_parenthesis_indices_in_operator_stack,
+                   current_index_of_operator_stack);
+      } else {
         /* Closed parenthesis encountered */
-        int last_open_parenthesis_index = stack_peek(open_parenthesis_indices_in_operator_stack);
+        int last_open_parenthesis_index =
+            stack_pop(open_parenthesis_indices_in_operator_stack);
         while (stack_length(operator_stack) > last_open_parenthesis_index) {
-          Operator top_of_the_stack = operator_stack[arrlen(operator_stack) - 1];
+          Operator top_of_the_stack = stack_pop(operator_stack);
           printf("%c\n", top_of_the_stack.symbol);
-          stack_delete_top(operator_stack);
         }
-        stack_delete_top(open_parenthesis_indices_in_operator_stack);
       }
     }
 
     else {
       /* our token is a regular token
        * just add it to the output buffer
-      */
+       */
       printf("%s\n", token);
     }
-
   }
 
-  /* while there are still operators in the stack, pop and add them to the output buffer */
+  /* while there are still operators in the stack, pop and add them to the
+   * output buffer */
   while (arrlen(operator_stack) != 0) {
-    Operator top_of_the_stack = arrlast(operator_stack);
+    Operator top_of_the_stack = stack_pop(operator_stack);
     printf("%c\n", top_of_the_stack.symbol);
-    arrdel(operator_stack, arrlen(operator_stack) - 1);
   }
 
   stack_free(open_parenthesis_indices_in_operator_stack);
@@ -121,11 +123,13 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
 
 int token_to_operator_index(const char *token) {
   size_t i;
-  
-  if (strlen(token) > 1) return -1;
+
+  if (strlen(token) > 1)
+    return -1;
 
   for (i = 0; i < sizeof(operators) / sizeof(Operator); ++i) {
-    if (operators[i].symbol == token[0]) return i;
+    if (operators[i].symbol == token[0])
+      return i;
   }
 
   return -1;
@@ -135,9 +139,12 @@ size_t get_precedence(Operator operator) {
   size_t i;
   size_t number_of_operators = sizeof(operators) / sizeof(Operator);
   for (i = 0; i < number_of_operators; ++i) {
-    if (operators[i].symbol == operator.symbol) return number_of_operators - i;
+    if (operators[i].symbol == operator.symbol)
+      return number_of_operators - i;
   }
-  
-  fprintf(stderr, "At get_precedence(): operator %c not found in the operators list", operator.symbol);
+
+  fprintf(stderr,
+          "At get_precedence(): operator %c not found in the operators list",
+          operator.symbol);
   exit(1);
 }
