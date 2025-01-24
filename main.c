e6bbcd8fc98498525e3c8215a019e87cbd8718c0
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-01-24 11:59:05 +0600

    added parenthesis stuff

--- main.c
+++ main.c
@@ -27,16 +27,19 @@ int token_to_operator_index(const char *token);
 size_t get_precedence(Operator operator);
 
 int main(void) {
-  char infix_tokens[][MAX_TOKEN_LENGTH] = {"1", "+", "2", "*", "3"};
+  char infix_tokens[][MAX_TOKEN_LENGTH] = {"(", "1", "+", "2", ")", "*", "3"};
 
-  to_reverse_polish_notation(infix_tokens, 5);
+  to_reverse_polish_notation(infix_tokens, 7);
 
   return 0;
 }
+
+
 void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t number_of_tokens) {
   size_t i;
 
   Operator *operator_stack = NULL;
+  int *open_parenthesis_indices_in_operator_stack = NULL;
 
   for (i = 0; i < number_of_tokens; ++i) {
     char *token = infix_tokens[i];
@@ -59,7 +62,7 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
         int x_is_right_associative = x.associativity == RIGHT;
         int x_precedence_is_less_than_y = precedence_of_x < precedence_of_y;
         int x_precedence_is_less_or_equal_to_y = precedence_of_x <= precedence_of_y;
-        
+
         if ((x_is_left_associative && x_precedence_is_less_or_equal_to_y) || (x_is_right_associative && x_precedence_is_less_than_y)) {
           /* pop y from the stack and add it to the output buffer */
           arrdel(operator_stack, arrlen(operator_stack) - 1);
@@ -73,6 +76,26 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
       arrput(operator_stack, x);
     }
 
+    else if (strlen(token) == 1 && (token[0] == '(' || token[0] == ')')) {
+      /* Our token is a parenthesis */
+      char parenthesis_token = token[0];
+      if (parenthesis_token == '(') {
+        /* Open parenthesis encountered */
+        int current_index_of_operator_stack = arrlen(operator_stack);
+        arrput(open_parenthesis_indices_in_operator_stack, current_index_of_operator_stack);
+      }
+      else {
+        /* Closed parenthesis encountered */
+        int last_open_parenthesis_index = arrlast(open_parenthesis_indices_in_operator_stack);
+        while (arrlen(operator_stack) > last_open_parenthesis_index) {
+          Operator top_of_the_stack = operator_stack[arrlen(operator_stack) - 1];
+          printf("%c\n", top_of_the_stack.symbol);
+          arrdel(operator_stack, arrlen(operator_stack) - 1);
+        }
+        arrdel(open_parenthesis_indices_in_operator_stack, arrlen(open_parenthesis_indices_in_operator_stack) - 1);
+      }
+    }
+
     else {
       /* our token is a regular token
        * just add it to the output buffer
@@ -84,11 +107,12 @@ void to_reverse_polish_notation(char infix_tokens[][MAX_TOKEN_LENGTH], size_t nu
 
   /* while there are still operators in the stack, pop and add them to the output buffer */
   while (arrlen(operator_stack) != 0) {
-    Operator top_of_the_stack = operator_stack[arrlen(operator_stack) - 1];
+    Operator top_of_the_stack = arrlast(operator_stack);
     printf("%c\n", top_of_the_stack.symbol);
     arrdel(operator_stack, arrlen(operator_stack) - 1);
   }
 
+  arrfree(open_parenthesis_indices_in_operator_stack);
   arrfree(operator_stack);
 }
 
